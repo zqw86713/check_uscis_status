2b9f3dc43650288252d3c5608b6f8a1c938dec63
Qingwei <qzhang.canada@gmail.com>
2024-12-28 10:01:37 -0500

    add

--- run_in_vnc.sh
+++ run_in_vnc.sh
@@ -6,5 +6,7 @@ export DISPLAY=:1
 # Set any other necessary environment variables for X11 or VNC session
 export XAUTHORITY=/root/.Xauthority
 
+source /root/.bashrc
+
 # Run the Python script
-/root/check_uscis_status/uscis/bin/python /root/check_uscis_status/main.py >> /root/check_uscis_status/logfile.log 2>&1
+/root/my_uscis_env/bin/python /root/check_uscis_status/main.py >> /root/check_uscis_status/logfile.log 2>&1
